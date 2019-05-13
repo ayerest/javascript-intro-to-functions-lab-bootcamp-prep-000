44619e4cd5c4cc60d23aba85a955516515388e9c
Iris S <irisiseve@gmail.com>
2019-05-13 23:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string) {
   } else if (string == string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string == "I love you, Grandma.") {
-    return "I lovey you, too."
+    return "I love you, too."
   }
 }
